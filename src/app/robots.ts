0c7f3ec8885b350d00b00d91b24484a612b996f2
inov8tr <bnsteiger@gmail.com>
2025-10-02 06:21:44 +0000

    Add sitemap.xml for SEO

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -3,11 +3,33 @@ import { SITE_URL } from "@/lib/seo";
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: {
-      userAgent: "*",
-      allow: "/",
-    },
-    sitemap: `${SITE_URL}/sitemap.xml`,
+    rules: [
+      {
+        userAgent: "*",
+        allow: "/",
+      },
+      {
+        userAgent: "Googlebot",
+        disallow: "*?lightbox=",
+      },
+      {
+        userAgent: ["AdsBot-Google-Mobile", "AdsBot-Google"],
+        disallow: ["/_api/*", "/_partials*", "/pro-gallery-webapp/v1/galleries/*"],
+      },
+      {
+        userAgent: "PetalBot",
+        disallow: "/",
+      },
+      {
+        userAgent: "AhrefsBot",
+        crawlDelay: 10,
+      },
+      {
+        userAgent: "Yeti",
+        allow: "/",
+      },
+    ],
+    sitemap: [`${SITE_URL}/sitemap.xml`],
     host: SITE_URL,
   };
 }
